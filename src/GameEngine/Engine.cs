d55dc765a9c10495ec3201dedb18a9470ab6f613
thousandVolts0 <maxeidert08@gmail.com>
2026-08-24 02:34:42 +0200

    Application and service initialization, basic window and gui rendering

---
+++ src/GameEngine/Engine.cs
@@ -0,0 +1,65 @@
+﻿using ImGuiNET;
+using Microsoft.Extensions.Logging;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+using System.Drawing;
+
+namespace GameEngine
+{
+    public class Engine
+    {
+        private IWindow _window;
+        private ILogger<Engine> _logger;
+        private ImGuiController _controller;
+        private IInputContext _input;
+        private GL _gl;
+
+        public Engine(IWindow window, ILogger<Engine> logger)
+        {
+            _window = window;
+            _logger = logger;
+        }
+
+        public void OnLoad()
+        {
+            _gl = _window.CreateOpenGL();
+            _input = _window.CreateInput();
+            _controller = new ImGuiController(_gl, _window, _input);
+
+            _logger.LogInformation("Engine loaded");
+        }
+
+        public void OnUpdate(double deltaTime)
+        {
+
+        }
+
+        public void OnRender(double deltaTime)
+        {
+            float dt = (float)deltaTime;
+
+            _controller.Update(dt);
+
+            _gl.ClearColor(Color.FromArgb(255, (int)(.45f * 255), (int)(.55f * 255), (int)(.60f * 255)));
+            _gl.Clear((uint)ClearBufferMask.ColorBufferBit);
+
+            ImGui.ShowDemoWindow();
+            _controller.Render();
+        }
+
+        public void OnClosing()
+        {
+            _controller.Dispose();
+            _input.Dispose();
+            _gl.Dispose();
+        }
+
+        public void OnResize(Vector2D<int> size)
+        {
+            _gl.Viewport(size);
+        }
+    }
+}
---
+++ src/GameEngine/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
+namespace GameEngine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceCollection services = new ServiceCollection();
+
+            var windowOptions = WindowOptions.Default with
+            {
+                API = GraphicsAPI.Default,
+                Size = new Vector2D<int>(1000, 600),
+                Title = "Game Engine",
+                VSync = true
+            };
+
+            var window = Window.Create(windowOptions);
+
+            services.AddLogging(config => config.AddConsole());
+            services.AddSingleton<Engine>();
+            services.AddSingleton<IWindow>(window);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var engine = serviceProvider.GetRequiredService<Engine>();
+
+            window.Load += engine.OnLoad;
+            window.Update += engine.OnUpdate;
+            window.Render += engine.OnRender;
+            window.Closing += engine.OnClosing;
+            window.Resize += engine.OnResize;
+
+            window.Run();
+            window.Dispose();
+        }
+    }
+}
